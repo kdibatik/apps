df611f676528777469ca28de88df72312b12246b
kdibatik <klasikdistribusiindonesia.apps@gmail.com>
2020-05-29 19:29:40 +0700

    Update KdiModel.php

--- application/models/KdiModel.php
+++ application/models/KdiModel.php
@@ -15,7 +15,8 @@ class KdiModel extends CI_Model
   }
 
   public function getProfileData ($username){
-    $this->db->select('A.username,A.laveluser,A.name,A.email,A.idgoogle,A.picture');
+    $month = date('m');
+    $this->db->select('A.username,A.laveluser,A.name,A.email,A.idgoogle,A.picture',((SELECT SUM(grandtotal) AS ttl FROM so_h WHERE sales=A.name AND MONTH(tgl)=$month) AS ttl))
     $this->db->from("{$this->user} A");
     $this->db->where('email', $username);
       $query = $this->db->get();
