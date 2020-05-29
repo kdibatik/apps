5fb095d30432f4688415c4800f517e5c521c69ae
kdibatik <klasikdistribusiindonesia.apps@gmail.com>
2020-05-29 19:52:38 +0700

    Update KdiModel.php

--- application/models/KdiModel.php
+++ application/models/KdiModel.php
@@ -4,7 +4,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class KdiModel extends CI_Model
 {
   public $user = 'user';
- 
+  public $soh = 'so_h';
 
 
   public function __construct()
@@ -16,10 +16,11 @@ class KdiModel extends CI_Model
 
   public function getProfileData ($username){
     $month = date('m');
-    $this->db->select('A.username,A.laveluser,A.name,A.email,A.idgoogle,A.picture',((SELECT SUM(grandtotal) AS ttl FROM so_h WHERE sales=A.name AND MONTH(tgl)=$month) AS ttl))
+    $this->db->select('A.username,A.laveluser,A.name,A.email,A.idgoogle,A.picture,SUM(B.grandtotal) as ttl')
     $this->db->from("{$this->user} A");
-    $this->db->where('email', $username);
-      $query = $this->db->get();
+    $this->db->join("{$this->soh} B", 'A.usernmae = B.sales');
+    $this->db->where('A.email', $username);
+    $query = $this->db->get();
     return $query->result();
   }
 }
