cc77c3bbe1e0bd2bce2e55096c2407986aa2bf35
kdibatik <klasikdistribusiindonesia.apps@gmail.com>
2020-06-01 13:24:27 +0700

    Update KdiModel.php

--- application/models/KdiModel.php
+++ application/models/KdiModel.php
@@ -181,11 +181,11 @@ class KdiModel extends CI_Model
         foreach($query->result() as $row){
           $datacolor["color"]=$row->warna;
           $ambildata=$this->getStockDetail($row->warna,$sts,$kodepro);
-          $datastc=array(
+          $datastc[]=array(
             $datacolor,
             'dataproduct'=>$ambildata
           );
-         
+         print_r($datastc);
         }
        
         $data = $datastc;
