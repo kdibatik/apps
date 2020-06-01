ff1873bb87fb12c4371c111e201f93e3b157b22f
kdibatik <klasikdistribusiindonesia.apps@gmail.com>
2020-06-01 12:44:09 +0700

    Update KdiModel.php

--- application/models/KdiModel.php
+++ application/models/KdiModel.php
@@ -25,7 +25,7 @@ class KdiModel extends CI_Model
     
     $this->db->select('A.username,A.laveluser,A.name,A.email,A.idgoogle,A.picture');
     $this->db->from("{$this->user} A");
-    $this->db->join("{$this->soh} B", 'A.username = B.sales','left');
+    // $this->db->join("{$this->soh} B", 'A.username = B.sales','left');
     $this->db->where('A.email', $username);
     // $this->db->where('Month(B.tgl)', $month);
     $query = $this->db->get();
