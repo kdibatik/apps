8f8e0a5e2efd5bbd71ee82af6ab0435ed85af739
kdibatik <klasikdistribusiindonesia.apps@gmail.com>
2020-06-18 01:29:55 +0700

    Update OmsetModel.php

--- application/models/OmsetModel.php
+++ application/models/OmsetModel.php
@@ -63,7 +63,7 @@ class OmsetModel extends CI_Model
     $this->db->where("C.email", $username);
     $this->db->where('A.tgl BETWEEN "'. date('Y-m-d', strtotime($first_day_this_year)). '" and "'. date('Y-m-d', strtotime($last_day_this_year)).'"');
     $this->db->group_by("DATE_FORMAT(A.tgl,'%M')","A.sales");
-    $this->db->order_by("DATE_FORMAT(A.tgl,'%M')");
+    $this->db->order_by("month(A.tgl)");
     $query = $this->db->get();
     return $query->result();
   }
