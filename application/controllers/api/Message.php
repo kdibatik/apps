fe54be95969be3b78cca5e7c648103696bf3a2da
kdibatik <klasikdistribusiindonesia.apps@gmail.com>
2020-06-24 17:49:40 +0700

    test7

---
+++ application/controllers/api/Message.php
@@ -0,0 +1,69 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
+class MessageApi extends REST_Controller {
+ 
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model("MessageModel","Kdimodel");
+   
+    }
+
+
+    public function getMessage_post(){
+
+        $username = $this->post("username");
+
+        $omsetData = $this->Kdimodel->getMessage($username);
+        
+        if (count($omsetData) == 0) {
+            $data["message"] = "User Tidak ditemukan";
+            $data["success"] = 0;
+        }else{
+            $data["success"] = 1;
+            $data["message"] = "Success Get Message";
+        }
+           
+        $data["data"] = $omsetData;
+        $this->response($data, REST_Controller::HTTP_OK);
+
+    }
+    public function saveMessage_post(){
+
+        $username = $this->post("username");
+        $picurl = $this->post("picurl");
+        $message = $this->post("message");
+        $title = $this->post("title");
+        $url = $this->post("url");
+
+        $tglskrg=date('Y-m-d H:i:s');
+
+        $data = array(
+            'username' => $username,
+            'title' => $kodepro,
+            'message' => $warna,
+            'picurl' => $ukuran,
+            'url' => $unitqty,
+            'tglsave' => $tglskrg,
+        );
+
+        $omsetData = $this->Kdimodel->postMessage($username,$data);
+        
+        if ($omsetData) {
+            $data["message"] = "User Tidak ditemukan";
+            $data["success"] = 0;
+        }else{
+            $data["success"] = 1;
+            $data["message"] = "Success Save Message";
+        }
+           
+        $data["data"] = $omsetData;
+        $this->response($data, REST_Controller::HTTP_OK);
+
+    }
+}
---
+++ application/models/MessageModel.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class MessageModel extends CI_Model
+{
+  public $user = 'user';
+  public $msg = 'message';
+
+  public function __construct()
+
+  {
+    parent::__construct();
+    $this->load->database();
+  }
+
+  public function postMessage($username,$data){
+    if($this->db->insert('message', $data)){
+        return TRUE;
+      }else{
+        return FALSE;
+      }
+  }
+
+  public function getMessage($username){
+    $this->db->select('A.email,A.message,A.picurl,A.title,A.url,A.tglsave,A.stsread');
+    $this->db->from("{$this->msg} A");
+    $this->db->where('A.email', $username);
+    $this->db->where('A.stsread', "0");
+    $query = $this->db->get();
+    return $query->result_array();
+  }
+
+}
