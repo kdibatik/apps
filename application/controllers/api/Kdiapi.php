fc85fb69ad77b0e2804072e1468648bff3dae2a5
kdibatik <klasikdistribusiindonesia.apps@gmail.com>
2020-06-27 13:42:55 +0700

    Update Kdiapi.php

--- application/controllers/api/Kdiapi.php
+++ application/controllers/api/Kdiapi.php
@@ -43,12 +43,13 @@ class KdiApi extends REST_Controller {
             $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
             $alphaLength=10;
             $filename=substr(str_shuffle($alphabet),0,$alphaLength).".jpg";
+            $namafilebanner = "uploads/banner/". $filename;
                // $filename = date('Y-m-d h:i:s').".jpg";
-                //$imagedata=str_replace(' ', '+', $linkgbr);
-                //base64_to_jpeg($imagedata, $filename);
+                $imagedata=str_replace(' ', '+', $linkgbr);
+                base64_to_jpeg($imagedata, $namafilebanner);
 
-                $decoded=base64_decode($linkgbr);
-                file_put_contents("uploads/profile/".$filename,$decoded);
+                // $decoded=base64_decode($linkgbr);
+                // file_put_contents("uploads/profile/".$filename,$decoded);
 
                 $data = array(
                     "picture" => $filename,
@@ -66,6 +67,22 @@ class KdiApi extends REST_Controller {
                 $this->response($resp, REST_Controller::HTTP_OK);
         }
               
+    }
+
+    function base64_to_jpeg($base64_string, $output_file) {
+        // open the output file for writing
+        $ifp = fopen($output_file, 'wb');
+    
+        // split the string on commas
+        // $data[ 0 ] == "data:image/png;base64"
+        // $data[ 1 ] == <actual base64 string>
+    
+        // we could add validation here with ensuring count( $data ) > 1
+        fwrite($ifp, base64_decode($base64_string));
+        // clean up the file resource
+        fclose($ifp);
+    
+        return $output_file;
     }
             
 }
